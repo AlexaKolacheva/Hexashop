64e7c5c386702a82164831ead46a92edf18d5fa1
Илана Енгай <ilanaengaj@MacBook-Air-Ilana.local>
2023-09-12 11:13:35 +0600

    Добавил новый код

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Callback
-from .forms import CallbackForm
+from django.shortcuts import render, redirect
+
+from .models import Callback, Review, Feedback, Comment, About
+from .forms import CallbackForm, ReviewForm, FeedbackForm, CommentForm
 
 
 def callbacks(request):
@@ -12,3 +13,40 @@ def callbacks(request):
     else:
         form = CallbackForm()
     return render(request, 'main/contact.html', {'form': form})
+
+
+# def feedback(request):
+#     feedbacks = Comment.objects.all()
+#     context = {
+#         'feedbacks': feedbacks
+#     }
+#     return render(request, 'main/reviews.html', context)
+
+
+def add_comment(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews')
+    else:
+        form = CommentForm()
+
+    return render(request, 'main/coment.html', {'form': form})
+
+
+def reviews(request):
+    comments = Comment.objects.all()
+
+    context = {
+        'comments': comments,
+    }
+
+    return render(request, 'main/reviews.html', context)
+
+def about(request):
+    abouts = About.objects.all()
+    context = {
+        'abouts': abouts
+    }
+    return render(request, 'main/about.html', context)
