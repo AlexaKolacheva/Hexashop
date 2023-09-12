32907d9453573fe64a7ac6672547297a09ce9f8a
Ibragim <gizuyev@gmail.com>
2023-09-12 11:25:22 +0600

    All

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
     path('callback/', views.callbacks, name='callback'),
     ]
+=======
+    path('', views.index, name='index'),
+]
+>>>>>>> 71f8e1e (authorization and html)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Callback
 from .forms import CallbackForm
 
+<<<<<<< HEAD
 
 def callbacks(request):
     if request.method == 'POST':
@@ -12,3 +13,9 @@ def callbacks(request):
     else:
         form = CallbackForm()
     return render(request, 'main/contact.html', {'form': form})
+=======
+def index(request):
+    return render(request, 'main/index.html')
+
+
+>>>>>>> 71f8e1e (authorization and html)
